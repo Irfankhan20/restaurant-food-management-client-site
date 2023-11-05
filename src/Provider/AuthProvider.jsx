8303a73a13db71ced196c6cb45c5322f6813f5e7
Irfankhan20 <sipathan96@gmail.com>
2023-11-05 23:48:20 +0600

    firebase install and authprovider create

---
+++ src/Provider/AuthProvider.jsx
@@ -0,0 +1,65 @@
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut, updateProfile } from "firebase/auth";
+import app from "../firebase/firebase.config";
+import { createContext, useEffect, useState } from "react";
+
+export const AuthContext = createContext();
+
+
+
+const auth = getAuth(app);
+const AuthProvider = ({ children }) => {
+
+    const [user, setUser] = useState(null);
+    const [loading,setLoading]=useState(true);
+
+    //create user
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+      };
+
+      //signIn user
+      const signInUser=(email,password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth,email,password);
+    }
+
+    //authStateChanged 
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+          setUser(currentUser);
+          setLoading(false);
+        });
+        return () => {
+          unsubscribe();
+        };
+      }, []);
+
+
+      //logout
+    const logOut = () =>{
+        setLoading(true);
+        return signOut(auth);
+    }
+
+    const updateUserProfile = (profile) => {
+        return updateProfile(auth.currentUser, profile);
+      };
+
+    const authInfo = {
+        user,
+        loading,
+        createUser,
+        signInUser,
+        logOut,
+        updateUserProfile
+    }
+
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBykNr6PEX038y-9kteHjWOKfSNuyQDM9Q",
+  authDomain: "assignment-eleven-website.firebaseapp.com",
+  projectId: "assignment-eleven-website",
+  storageBucket: "assignment-eleven-website.appspot.com",
+  messagingSenderId: "1035774310025",
+  appId: "1:1035774310025:web:984b7100be3111fd5cf858"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
