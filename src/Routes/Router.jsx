a8b9550dd6b4e9bb12a081bc4f36ceaafe12f9ec
Irfankhan20 <sipathan96@gmail.com>
2023-11-06 01:29:51 +0600

    error page complete

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -5,12 +5,14 @@ import AllFoodItems from "../AllFoodItems/AllFoodItems";
 import Blog from "../Blog/Blog";
 import Login from "../Login/Login";
 import Register from "../Register/Register";
+import ErrorPage from "../ErrorPage/ErrorPage";
 
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <Main></Main>,
+        errorElement: <ErrorPage/>,
         children: [
           {
               path: '/',
