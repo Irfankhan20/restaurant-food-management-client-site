2788f1c3db9092423f797fedf7d7ed6ae04112a2
Irfankhan20 <sipathan96@gmail.com>
2023-12-13 19:52:37 +0600

    2 new features section add

---
+++ src/Home/CustomerSupport.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import MessengerCustomerChat from 'react-messenger-customer-chat';
+
+
+const CustomerSupport = () => {
+    return (
+        <div>
+            <div>
+                <MessengerCustomerChat
+                    pageId="104544296085994"
+                    appId="388812903566987"
+                   
+                />,
+            </div>
+        </div>
+    );
+};
+
+export default CustomerSupport;
